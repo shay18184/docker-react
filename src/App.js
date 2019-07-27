cfd3a8c2d23d19912977ad2737d8d00d8b35098a
Shay Libo <shay.libo@ironsrc.com>
2019-07-27 16:14:18 +0300

    change text in feature branch

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - changed
+          this text changed in feature
         </a>
       </header>
     </div>
